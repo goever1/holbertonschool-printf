ee87131d8748a9642d4d4799b69ca1c826e4e23c
root <jorgefernandoochoa1@hotmail.com>
2023-03-31 09:37:37 -0700

    1.0

---
+++ caseC.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+ * r-case - r case function
+ * @ap: string
+ * @buff: buffer
+ * @print_len: position of buff
+ * Return: Last position on buf
+ */
+int r_case(va_list ap, char *buff, int print_len)
+{
+	char *s;
+	char *aux;
+	int s_len;
+	
+	aux = va_arg(ap, char*);
+	if (aux == NULL)
+		aux = "(null)";
+	s_len = _strlen(aux);
+	s = malloc ((sizeof(char) * s_len) + 1);
+	if (s == NULL)
+		return (1);
+	_strcpy(s, aux);
+	rev_string(s);
+	print_len = replace(buff, s, print:len);
+	free(s);
+	return (print_len);
+}
+/**
+ * R_case - R case function
+ * @ap: string
+ * @buff: buffer
+ * @print_len: position fo buff
+ * Return: last position on buff
+ */
+int R_case(va_list ap, char *buff, int print_len)                                                                                                                                               
+{
+	char *s;
+	char *aux;
+	int s_len;
+	
+	aux = va_arg(ap, char*);
+	if (aux == NULL)
+		aux = "(null)";
+	s_len = _strlen(aux);
+	s = malloc ((sizeof(char) * s_len) + 1);
+	if (s == NULL)
+		return (1);
+	_strcpy(s, aux);
+	rev_string(s);
+	print_len = replace(buff, s, print:len);
+	free(s);
+	return (print_len);
+}
