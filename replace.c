389476ca7a02f9322115342723bb3f6cf3771fd8
root <jorgefernandoochoa1@hotmail.com>
2023-03-31 16:41:46 -0700

    fixing sintax error

--- replace.c
+++ replace.c
@@ -1,5 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
 /**
  * replace - puts s string in pointer buffer
  * @buffer: buffer
@@ -15,26 +14,6 @@ int replace(char *buffer, char *s, int print_len)
 	{
 		buffer[i] = s[i];
 		print_len += 1;
-=======
-
-/**
- * replace - puts s string on pointer buffer
- * @buffer: Destination buffer
- * @s: String to put
- * @print_len: Final position
- * Return: Final position
- */
-
-int replace(char *buffer, char *s, int print_len)
-{
-	int i = 0;
-
-	while (s && s[i])
-	{
-		buffer[i] = s[i];
-		print_len += 1;
-		i++;
->>>>>>> eee440b3a68610615a835d782f65e7a458cc0e17
 	}
 	return (print_len);
 }
