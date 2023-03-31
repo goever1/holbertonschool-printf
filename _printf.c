c996d75a8ab1ced4ee714d8ff4f3cbe5e47f067c
root <jorgefernandoochoa1@hotmail.com>
2023-03-31 09:58:32 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _printf - printf function
+ * @format: string to print
+ * Return: -1 when fail, stringht length when success
+ */
+int _printf(char *format, ...)
+{
+	int i = 0;
+	size_t print_len = 0;
+	va_list ls;
+	char buffer[2048];
+	int (*con)(va_list, char *, int);
+
+	if (format == NULL || (format == [0] == '%' && format [1] == '\0')
+			|| (format[0] == '\\' && format[1] == '\0'))
+		return (-1);
+	va_start(ls, format);
+	for (; format[i] != '\0'; ++i)
+	{
+		if (format[i] != '%')
+		{
+			buffer[print_len] = format[i];
+			print_len += 1;
+		}
+		con = get_print_case(&(format[i + 1]));
+		if (con != NULL)
+			print_len = con(ap, &buffer[print_len], print_len);
+		else
+		{
+			buffer[print_len] = format[1];
+			print_len += 1;
+		}
+	}
+	write(1, buffer, print_len);
+	va_end(ls);
+	return (print_len);
+}
