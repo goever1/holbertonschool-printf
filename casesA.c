a57772fee70c1f14822e4cd974b38cef7d037011
root <jorgefernandoochoa1@hotmail.com>
2023-04-01 16:09:52 -0700

    fix betty error

--- casesA.c
+++ casesA.c
@@ -35,7 +35,6 @@ int c_case(va_list ls, char *buff, int print_len)
 int s_case(va_list ls, char *buff, int print_len)
 {
 	char *str = va_arg(ls, char*);
-	
 	if (!str)
 		str = "(null)";
 	return (replace(buff, str, print_len));
@@ -56,68 +55,29 @@ int dec_case(va_list ls, char *buff, int print_len)
 	int s_len, num;
 
 	num = va_arg(ls, int);
-	/**
-	 *Retrieve the integer argument from the argument list
-	 */
 	aux = malloc(sizeof(char) * 35);
-	/**
-	 *Allocate memory to store the integer converted to a string
-	 */
 	if (aux == NULL)
-		/**
-		 *Check if memory allocation was successful
-		 */
 		return (1);
-	/**
-	 *Return an error
-	 */
 	aux = _itoa(num, aux, 10);
-	/**
-	 *Convert the integer to a string
-	 */
 	s_len = _strlen(aux);
-	/**
-	 *Calculate the length of the string
-	 */
 	s = malloc((sizeof(char) * s_len) + 1);
-	/**
-	 *Allocate memory to store the string
-	 */
 	if (s == NULL)
-		/**
-		 *Check if memory allocation was successful
-		 */
 		return (1);
-	/**
-	 *Return an error
-	 */
 	_strcpy(s, aux);
-	/**
-	 *Copy the string to the allocated memory
-	 */
 	print_len = replace(buff, s, print_len);
 	return (print_len);
-	/**
-	 *Replace the format specifier with the string in the buffer
-	 */
 	free(s);
-	/**
-	 *Free the allocated memory
-	 */
 	free(aux);
 }
-	/**
-	 * perc_case - function for % case
-	 * @ls: Unused
-	 * @buff: buffer
-	 * @print_len: Actual position of buff
-	 * Return: Last position on buff
-	*/
+/**
+ *  perc_case - function for % case
+ *  @ls: Unused
+ *  @buff: buffer
+ *  @print_len: Actual position of buff
+ *  Return: Last position on buff
+ */
 int perc_case(va_list __attribute__((unused)) ls, char *buff, int print_len)
 {
-	/**
-	 * Allocate memory for a single character and a null terminator
-	 */
 	char *c;
 
 	c = malloc((sizeof(char) + 1));
@@ -141,54 +101,18 @@ int bin_case(va_list ls, char *buff, int print_len)
 	char *s;
 	char *aux;
 	int s_len, num;
-/**
- *Get the integer argument from the variable arguments list
- */
+
 	num = va_arg(ls, int);
-
-/**
- *Allocate memory for a string representation of the binary number
- */
 	aux = malloc(sizeof(char) * 35);
-
-		/**
-		 *Check if allocation was successful
-		 */
 	if (aux == NULL)
 		return (1);
-/**
- *Convert the integer to a binary string representation
- */
 	aux = _itoa(num, aux, 2);
-/**
-*Get the length of the binary string
-*/
 	s_len = _strlen(aux);
-
-/**
- *Allocate memory for the final string representation of the binary number
- */
 	s = malloc((sizeof(char) * s_len) + 1);
-/**
- *Check if allocation was successful
- */
 	if (s == NULL)
 		return (1);
-
-/**
- *Copy the binary string to the final string representation
- */
 	_strcpy(s, aux);
-
-   /**
- *Replace the corresponding placeholder in the buffer with
- *the final string representation
- */
 	print_len = replace(buff, s, print_len);
-
-/**
- *Free the memory allocated for the string representations
- */
 	free(s);
 	free(aux);
 	return (print_len);
